1c65244a70755f33230bab0a0a2088b4ff265496
Victor Marrero Shimko <vmarreros@uclv.cu>
2018-09-05 16:21:56 -0400

    debug in production False

--- service_django/application/settings/application/production.py
+++ service_django/application/settings/application/production.py
@@ -2,7 +2,7 @@ from django.utils.translation import ugettext_lazy as _
 import os
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')[1:-1]
